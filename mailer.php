af44d6bfa5bcbde37148d9c459995fe456805aeb
SergioBonatto <bonatto@tutanota.com>
2020-06-29 16:52:17 -0400

    correção email, faltava dot com

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "tenentebonatto@gmail";
+        $recipient = "tenentebonatto@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
